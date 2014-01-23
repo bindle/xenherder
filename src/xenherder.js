fd51571b202ec75067e4cf0658d725510c21052a
David M. Syzdek <syzdek@bindlebinaries.com>
2014-01-23 00:35:09 -0900

    Adding initial javascript.

---
+++ src/xenherder.js
@@ -0,0 +1,26 @@
+
+function xenherder_init()
+{
+   // adds table sorting using JQuery/tablesorter
+   // overrides sort function so that empty cells
+   // are always evaluated as -1
+   $.tablesorter.formatInt = function (s)
+   {
+      var i = parseInt(s);
+      return (isNaN(i)) ? -1 : i;
+   };
+   $.tablesorter.formatFloat = function (s)
+   {
+      var i = parseFloat(s);
+      return (isNaN(i)) ? -1 : i;
+   };
+   $("table").tablesorter();
+
+   return;
+}
+
+
+/* for other browsers */
+window.onload = xenherder_init;
+
+/* end of JavaScript */
